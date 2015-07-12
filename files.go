a511b66a3fdad01f57a08cdc59cae7531159ca59
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-07-12 16:10:14 +0900

    Key file is encrypted.

--- files.go
+++ files.go
@@ -18,28 +18,58 @@ func PasswordsFile() (*os.File, error) {
 	return os.Open(path)
 }
 
-func Key() ([]byte, error) {
-	res, err := ioutil.ReadFile(KEY_PATH)
+func Key(passphrase []byte) ([]byte, error) {
+	f, err := os.Open(KEY_PATH)
 	if err != nil {
 		return nil, err
 	}
-	if len(res) != 32 {
-		return nil, fmt.Errorf("key length should be 256bit. But got %d", len(res))
+	defer f.Close()
+
+	passkey := passphraseToAESKey(passphrase)
+	c, err := NewCryptor(f, passkey)
+	if err != nil {
+		return nil, err
 	}
-	return res, nil
+
+	key, err := ioutil.ReadAll(c)
+	if err != nil {
+		return nil, err
+	}
+	if len(key) != 32 {
+		return nil, fmt.Errorf("key length should be 256bit. But got %d", len(key))
+	}
+
+	return key, nil
 }
 
-func NewKey() ([]byte, error) {
+func NewKey(passphrase []byte) ([]byte, error) {
 	key := make([]byte, 32)
 	_, err := rand.Read(key)
 	if err != nil {
 		return nil, err
 	}
 
-	err = ioutil.WriteFile(KEY_PATH, key, 0400)
+	f, err := os.OpenFile(KEY_PATH, os.O_WRONLY|os.O_CREATE, 0400)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	passKey := passphraseToAESKey(passphrase)
+	c, err := NewCryptor(f, passKey)
+	if err != nil {
+		return nil, err
+	}
+	_, err = c.Write(key)
 	if err != nil {
 		return nil, err
 	}
 
 	return key, nil
+}
+
+func passphraseToAESKey(passphrase []byte) []byte {
+	res := make([]byte, 32)
+	copy(res, passphrase)
+
+	return res
 }
